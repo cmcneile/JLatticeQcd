899403dd72cdce7243badb29512f292ceb3768cc
Craig McNeile <craig_mcneile@yahoo.com>
2012-11-15 22:12:01 +0100

    I have added javadoc stuff

--- Global.java
+++ Global.java
@@ -1,11 +1,28 @@
 public class Global {
 
+    /**
+     * SU(GROUP)
+     */
     public static int GROUP = 2 ;
+
+    /**
+     * Dimension of spacetime
+     */
     public static int DIM   = 4;
+
+/**
+ * Description of the variable here.
+ */
     public static int SIZE = 8 ;
 
+/**
+ * Beta value -- a parameter in the Wilson action
+ */
     public static double beta = 2.3 ;
 
+    /**
+     * Dimension of the lattice
+     */
     public static int[] shape = {2 ,2 ,2 ,2 } ;
 
     /* other shapes are allowed but 
